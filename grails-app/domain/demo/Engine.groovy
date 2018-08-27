cd0c913b21ac5ed26a95dbfeab4f5772c5c00af6
Xavier <xavier@virtualsw.com>
2018-08-27 08:40:01 +0200

    class domain and yml config

---
+++ grails-app/domain/demo/Engine.groovy
@@ -0,0 +1,12 @@
+package demo
+
+import grails.gorm.MultiTenant
+
+class Engine implements MultiTenant<Engine> {
+
+    Integer cylinders
+
+    static constraints = {
+        cylinders nullable: false
+    }
+}
---
+++ grails-app/domain/demo/Vehicle.groovy
@@ -0,0 +1,15 @@
+package demo
+
+import grails.gorm.MultiTenant
+
+class Vehicle implements MultiTenant<Vehicle> {
+
+    String model
+    Integer year
+
+    static hasMany = [engines: Engine]
+    static constraints = {
+        model blank: false
+        year min: 1980
+    }
+}
